8b9e12d5274bd9ba6c95055cbd5d369780f1074d
Gwen <gfernandez@wesleyan.edu>
2017-10-06 15:20:01 -0400

    all plots added

--- mortality_app/server.R
+++ mortality_app/server.R
@@ -17,42 +17,75 @@ function(input, output) {
         + theme_base() + ylab("Death Rate") + ggtitle("Annual Mortality by Drug and Alcohol Use, 199-2015"))
     })
     
-    YearCoD = reactive({
-      InputYear = input$slideYear
-      edit_data = as.data.frame(raceCoDInput())
-      edit_data = edit_data[Year == InputYear,]
+    raceInput = reactive({
+      input$racebutton
+    })
+    YearRace_Input = reactive({
+      input$slideYear
     })
     
-    raceCoDInput = reactive({
-      buttonCause = input$CoDbutton
-      if (buttonCause == "Drug-Induced") {
-        return (droplevels(subset(GenderAge, DrugAlc_induced_causes == "Drug-Induced Causes")))
-      } else {
-        return (droplevels(subset(GenderAge, DrugAlc_induced_causes == "Alcohol-Induced Causes")))
-      }
-    })
-    
-    output$annualmortrate_Gender = renderPlot({
-      plot3 = deaths_Gender
-      (ggplot(data=deaths_Gender, aes(x=Year, y=annualrate, group=Gender, color=Gender)) + geom_point(size=1.5) + geom_line(size=1) 
-        + scale_color_manual(values = c("#1c9099", "#e34a33"))
-        + ylab("Death Rates") + theme_base() + ggtitle("Annual Mortality Rates by Gender, 1999-2015"))
+    filter_RaceYr = reactive({
+      RaceAge %>% filter(Race == raceInput()&Year == YearRace_Input())
     })
     
     output$annualmortrate_Age = renderPlot({
-      edit_data = as.data.table(YearCoD())
-      edit_data = edit_data[, .(DeathsPrct = ((sum(as.numeric(Deaths))/((sum(as.numeric(Population)))*1000))/365)*100), by = .(Year, Age_grps)]
-      plot4 = as.data.frame(edit_data)
-      (ggplot(data=edit_data, aes(x=Year, y=DeathsPrct, fill=Age_grps)) + geom_col(position = "dodge")
+      edit_data = as.data.frame(filter_RaceYr())
+      edit_data = edit_data %>% group_by(Age_grps) %>% summarise(rate = sum(as.numeric(Deaths))/sum(as.numeric(Population)))
+      (ggplot(data=edit_data, aes(x=Age_grps, y=rate, fill=Age_grps)) + geom_col(position = "dodge")
         + theme_base())
+    })
+    
+    output$DrugVAlc_raceYear = renderPlot({
+      plot4 = as.data.frame(filter_RaceYr())
+      plot4 = plot4 %>% group_by(DrugAlc_induced_causes) %>% summarise(rate = sum(as.numeric(Deaths))/sum(as.numeric(Population)))
+      (ggplot(data = plot4, aes(x = DrugAlc_induced_causes, y = rate, fill = DrugAlc_induced_causes))
+        + geom_col())
+    })
+    
+    output$AllDeaths_Race = renderPlot ({
+      plot5 = as.data.frame(RaceAge)
+      plot5 = plot5 %>% filter(Race == raceInput()) %>% group_by(Year) %>% summarise(rate = sum(as.numeric(Deaths))/sum(as.numeric(Population)))
+      (ggplot(data = plot5, aes(x = Year, y = rate, group = 1)) + geom_point() + geom_line() + theme_base()) 
+    })
+    
+    YearGen_Input = reactive ({
+      input$slideYearGen
+    })
+    
+    Gen_Input = reactive({
+      input$genderbutton
+    })
+    
+    filterGen = reactive({
+      GenderAge %>% filter(Year == YearGen_Input()&Gender == Gen_Input())
+    })
+    
+    output$annualmortrate_Gender = renderPlot({
+      plotDeathsGender = GenderAge %>% filter(Gender == Gen_Input()) %>% group_by(Year) %>% summarise(rate = sum(as.numeric(Deaths))/sum(as.numeric(Population)))
+      (ggplot(data=plotDeathsGender, aes(x=Year, y=rate, group=1)) + geom_point(size=1.5) + geom_line(size=1) 
+        + ylab("Death Rate") + theme_base() + ggtitle("Annual Mortality Rates by Gender, 1999-2015"))
+    })
+    
+    output$DrugVAlc_GenYear = renderPlot({
+      DrugValc_gender = as.data.table(filterGen())
+      DrugValc_gender = DrugValc_gender %>% group_by(DrugAlc_induced_causes) %>% summarise(rate = sum(as.numeric(Deaths))/sum(as.numeric(Population)))
+      (ggplot(data = DrugValc_gender, aes(x=DrugAlc_induced_causes, y=rate, fill=DrugAlc_induced_causes)) + geom_col() + theme_base())
+    })
+    
+    output$Age_GenYear = renderPlot({
+      Gender_age = as.data.table(filterGen())
+      Gender_age = Gender_age %>% group_by(Age_grps) %>% summarise(rate = sum(as.numeric(Deaths))/sum(as.numeric(Population)))
+      (ggplot(data = Gender_age, aes(x=Age_grps, y=rate, fill=Age_grps)) + geom_col() + theme_base())
     })
     
     YearInput = reactive({
       year = input$selYear
-      droplevels(subset(byST_annualrate, Year %in% year))
+      Totals_cause %>% filter(Year == year)
     })
+    
     output$geochart = renderGvis({
-      states_year <- as.data.frame(YearInput())
+      states_year = as.data.frame(YearInput())
+      states_year = states_year %>% group_by(State) %>% summarise(rate = sum(as.numeric(Deaths))/sum(as.numeric(Population)))
       gvisGeoChart(states_year,"State", "rate",
                    options=list(region="US", displayMode="regions", 
                                 resolution="provinces",
--- mortality_app/ui.R
+++ mortality_app/ui.R
@@ -26,20 +26,37 @@ fluidPage(
                    htmlOutput("geochart")
                  )
         ),
-        tabPanel("Who is most affected?",
+        tabPanel("Racial Breakdown",
                    fluidRow(
                      column(6,
-                            radioButtons("CoDbutton", label = h3("Select a Cause of Death:"),
-                                         choices = list("Drug-Induced" = 1, "Alcohol-Induced" = 2), 
-                                         selected = 1),
+                            radioButtons("racebutton", label = h3("Select Race/Ethnicity:"),
+                                         choices = list("Black or African American", "American Indian or Alaska Native",
+                                                        "Asian or Pacific Islander", "White", "Hispanic or Latino"), 
+                                         selected = "Black or African American"),
                             sliderInput(inputId = "slideYear", label = h3("Please Select A Year:"), min = 1999, 
                                         max = 2015, value = c(2015), step = 1, round = TRUE, sep = "", ticks = TRUE,width = 500)
                      )
                     ),
         mainPanel(
-          htmlOutput("raceRatesCoD"),
+          plotOutput("annualmortrate_Age"),
+          plotOutput("DrugVAlc_raceYear"),
+          plotOutput("AllDeaths_Race")
+        )
+        ),
+        tabPanel("Breakdown by Gender",
+          fluidRow(
+            column(6,
+                   radioButtons("genderbutton", label = h3("Select Sex:"),
+                                choices = list("Female", "Male"), 
+                                selected = "Female"),
+                   sliderInput(inputId = "slideYearGen", label = h3("Please Select a Year"), min = 1999,
+                               max = 2015, value = c(2015), step=1, round = TRUE, sep = "", ticks = TRUE, width = 500)
+                   )
+          ),
+        mainPanel(
           plotOutput("annualmortrate_Gender"),
-          plotOutput("annualmortrate_Age")
+          plotOutput("DrugVAlc_GenYear"),
+          plotOutput("Age_GenYear")
         )
         )
       )
